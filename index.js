687025f1c8a64647b8b30b38a247dab7b4cc8c7a
Karolina <kmieczkowska75@gmail.com>
2021-06-06 14:30:33 +0100

    Add overlay functionality

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-document.querySelector('.button').href = 'mailto:' + new Array("kmieczkowska75","gmail.com").join("@") + '\'>';
+const nav = document.querySelector('nav');
+const burgerMenu = document.querySelector('.burger-menu button');
+const sideDrawer = document.querySelector('.side-drawer');
+const backDrop = document.querySelector('.backdrop');
+const crossIcon = document.querySelector('.cross-icon');
+const navLinks = document.querySelectorAll('.side-drawer li');
+const projectsActiveLink = document.querySelector('.active-projects-link')
+
+const activate = () => {
+    sideDrawer.classList.add('active');
+    backDrop.classList.add('active');
+}
+
+const deActivate = () => {
+    sideDrawer.classList.remove('active');
+    backDrop.classList.remove('active');
+}
+
+burgerMenu.addEventListener('click', (event) => {
+    activate();
+})
+
+backDrop.addEventListener('click', (event) => {
+    deActivate();
+})
+
+crossIcon.addEventListener('click', (event) => {
+    deActivate();
+})
+
+window.addEventListener("hashchange", function () {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+});
+
+
+
+
+
+
+
